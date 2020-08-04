969cef920474a60b6c85078f5b7ece76914296d1
Saugat Poudel <sapoudel@ucsd.edu>
2020-08-03 19:24:30 -0700

    removed dictionary intermediates.

--- pymodulon/io.py
+++ pymodulon/io.py
@@ -9,35 +9,6 @@ Functions for reading and writing model into files.
 #TODO: should only save subset of the model. i.e. things like colors can be discarded to reduce file size.
 #TODO: Change M to S when core is changed
 
-
-def model_to_dict(model) -> dict:
-    """
-
-    Convert model to dictionary with serialized objects.
-    Parameters
-    ----------
-    model : ICA model
-        ICA model to be converted to dict. Must contain M and A matrix
-
-    Returns
-    -------
-    model_dict: dict
-        dictionary version of ICA model object.
-
-    """
-
-    if model.A == None or model.M == None:
-        raise ValueError('The model must include the M and the A matrix.')
-
-    #serialize pandas DataFrames
-    for key, val in model.__dict__.items():
-        if isinstance(val, pd.core.frame.DataFrame):
-            model.__dict__.update({key: val.to_json()})
-        elif isinstance(val, set):
-            model.__dict__.update({key: list(val)})
-    return model.__dict__
-
-
 def save_to_json(model: IcaData, fname: str):
     """
 
@@ -50,11 +21,21 @@ def save_to_json(model: IcaData, fname: str):
        path to json file where the model will be saved
 
     """
+
+    if model.A is None or model.M is None:
+        raise ValueError('The model must include the M and the A matrix.')
+
+    #serialize pandas DataFrames and sets
+    for key, val in model.__dict__.items():
+        if isinstance(val, pd.core.frame.DataFrame):
+            model.__dict__.update({key: val.to_json()})
+        elif isinstance(val, set):
+            model.__dict__.update({key: list(val)})
+
     if not fname.endswith('.json'):
         fname = fname += '.json'
-
     with open(fname, 'w') as fp:
-        json.dump(model_to_dict(model), fp)
+        json.dump(model.__dict__, fp)
 
 def load_json_model(filename: Union[str, file]) -> IcaData:
     """
@@ -83,23 +64,4 @@ def load_json_model(filename: Union[str, file]) -> IcaData:
         df = pd.DataFrame(serial_data[dtype])
         serial_data.update({dtype: df})
 
-    return model_from_dict(serial_data)
-
-def model_from_dict(object: dict) -> IcaData:
-    """
-
-    Parameters
-    ----------
-    object: dict
-        dictionary containing the ICA model, must contain M and A matrices
-
-    Returns
-    -------
-    ica_model:
-        ICA model as represented in the dictionary
-
-    """
-    if 'A' not in  object or 'M' not in object:
-        raise ValueError('The model must include the M and the A matrix.')
-
-    model = IcaData(**object)
+    return ICA_Data(**serial_data)
