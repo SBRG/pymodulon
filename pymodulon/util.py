306534defc017a06a520e15459952b40fc8c5b7f
Siddharth Chauhan <github.chauhan.siddharth@gmail.com>
2020-08-13 15:33:01 -0700

    style: PEP8 changes

--- pymodulon/util.py
+++ pymodulon/util.py
@@ -8,6 +8,7 @@ import os
 ImodName = Union[str, int]
 Data = Union[pd.DataFrame, os.PathLike]
 
+
 def _check_table(table: Data, name: str, index: Optional[Collection] = None):
     # Set as empty dataframe if not input given
     if table is None:
@@ -19,26 +20,30 @@ def _check_table(table: Data, name: str, index: Optional[Collection] = None):
             table = pd.read_json(table)
         except ValueError:
             try:
-                sep = lambda s: '\t' if 'tsv' in s else ','
+                def sep(s): return '\t' if 'tsv' in s else ','
                 table = pd.read_csv(table, index_col=0, sep=sep(table))
             except FileNotFoundError:
-                raise TypeError('{}_table must be a pandas DataFrame filename or a valid JSON string'.format(name))
+                raise TypeError('{}_table must be a pandas DataFrame '
+                                'filename or a valid JSON string'.format(name))
 
     if isinstance(table, pd.DataFrame):
-        #dont run _check_table_helper if no index is passed
-        return table if index is None else _check_table_helper(table, index, name)
+        # dont run _check_table_helper if no index is passed
+        return table if index is None else _check_table_helper(table, index,
+                                                               name)
     else:
-        raise TypeError('{}_table must be a pandas DataFrame filename or a valid JSON string'.format(name))
+        raise TypeError('{}_table must be a pandas DataFrame '
+                        'filename or a valid JSON string'.format(name))
 
 
-
-def _check_table_helper(table: pd.DataFrame, index: Optional[Collection], name: ImodName):
+def _check_table_helper(table: pd.DataFrame, index: Optional[Collection],
+                        name: ImodName):
     if table.shape == (0, 0):
         return pd.DataFrame(index=index)
     # Check if all indices are in table
     missing_index = list(set(index) - set(table.index))
     if len(missing_index) > 0:
-        warn('Some {} are missing from the {} table: {}'.format(name, name, missing_index))
+        warn('Some {} are missing from the {} table: {}'.format(name, name,
+                                                                missing_index))
 
     # Remove extra indices from table
     table = table.loc[index]
@@ -49,7 +54,8 @@ def compute_threshold(ic: pd.Series, dagostino_cutoff: float):
     """
     Computes D'agostino-test-based threshold for a component of an M matrix
     :param ic: Pandas Series containing an independent component
-    :param dagostino_cutoff: Minimum D'agostino test statistic value to determine threshold
+    :param dagostino_cutoff: Minimum D'agostino test statistic value
+        to determine threshold
     :return: iModulon threshold
     """
     i = 0
@@ -60,7 +66,7 @@ def compute_threshold(ic: pd.Series, dagostino_cutoff: float):
     # Compute k2-statistic
     k_square, p = stats.normaltest(ic)
 
-    # Iteratively remove gene with largest weight until k2-statistic is below cutoff
+    # Iteratively remove gene w/ largest weight until k2-statistic < cutoff
     while k_square > dagostino_cutoff:
         i -= 1
         k_square, p = stats.normaltest(ic.loc[ordered_genes.index[:i]])
