bd5c8438651ffd60424f71862e10900411355141
Siddharth Chauhan <github.chauhan.siddharth@gmail.com>
2020-08-14 14:54:05 -0700

    feat: add ax, figsize parameters

--- pymodulon/visualization.py
+++ pymodulon/visualization.py
@@ -1,6 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from matplotlib.axes._subplots import Subplot
 from pymodulon.core import IcaData
 from pymodulon.util import *
 
@@ -11,13 +12,26 @@ sns.set_style('whitegrid')
 ########################
 
 
-def plot_samples_bar(ica_data: IcaData, imodulon: ImodName):
+def plot_samples_bar(ica_data: IcaData, imodulon: ImodName,
+                     ax: Optional[Subplot] = None,
+                     figsize: Tuple[float, float] = (15, 2)):
     """
-    :param ica_data:
+    :param ica_data: iModulon Data container object
     :param imodulon: Name of iModulon
+    :param ax: matplotlib Axes instance to output plot onto
+    :param figsize: Size of output plot
     """
+
+    # Check that iModulon exists
     if imodulon not in ica_data.imodulon_names:
         raise ValueError('Component does not exist: {}'.format(imodulon))
 
-    print(ica_data)  # placeholder
-    print(imodulon)  # placeholder
+    if ax == None:
+        fig, ax = plt.subplots(figsize=figsize)
+
+    # Get ymin and ymax
+    ymin = ica_data.A.loc[imodulon].min()-3
+    ymax = ica_data.A.loc[imodulon].max()+3
+
+    print(ica_data)  # placeholder, remove later
+    print(imodulon)  # placeholder, remove later
