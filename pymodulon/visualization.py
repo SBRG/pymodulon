89e12d3ae5c3e44d73820b9fa51e4db942a5cf06
Siddharth Chauhan <github.chauhan.siddharth@gmail.com>
2020-08-14 15:47:54 -0700

    feat: add project parameter, minor fixes

--- pymodulon/visualization.py
+++ pymodulon/visualization.py
@@ -13,11 +13,13 @@ sns.set_style('whitegrid')
 
 
 def plot_samples_bar(ica_data: IcaData, imodulon: ImodName,
+                     project: Optional[str] = None,
                      ax: Optional[Subplot] = None,
                      figsize: Tuple[float, float] = (15, 2)):
     """
     :param ica_data: iModulon Data container object
     :param imodulon: Name of iModulon
+    :param project: Name of project (from metadata)
     :param ax: matplotlib Axes instance to output plot onto
     :param figsize: Size of output plot
     """
@@ -26,7 +28,7 @@ def plot_samples_bar(ica_data: IcaData, imodulon: ImodName,
     if imodulon not in ica_data.imodulon_names:
         raise ValueError('Component does not exist: {}'.format(imodulon))
 
-    if ax == None:
+    if ax is None:
         fig, ax = plt.subplots(figsize=figsize)
 
     # Get ymin and ymax
@@ -40,6 +42,10 @@ def plot_samples_bar(ica_data: IcaData, imodulon: ImodName,
     other.reset_index(inplace=True)
     ax.bar(range(len(other)), ica_data.A.loc[imodulon, other['sample_id']],
            width=1, linewidth=0, align='edge', label='Previous Experiments')
+
+    # Draw lines to discriminate between projects
+    p_lines = other.project_name.drop_duplicates().index.tolist() \
+        + [len(other), len(ica_data.sample_table)]
 
     print(ica_data)  # placeholder, remove later
     print(imodulon)  # placeholder, remove later
