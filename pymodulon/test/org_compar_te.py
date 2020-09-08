0f8bf2b15b0271366f85f6766cfd517c1ea861da
Reosu <rsyoo@ucsd.edu>
2020-09-08 16:21:22 -0700

    Modified _pull_bbh and compare_ica for single file

--- pymodulon/test/org_compar_te.py
+++ pymodulon/test/org_compar_te.py
@@ -10,26 +10,12 @@ from pymodulon.compare import *
 class MyTestCase(unittest.TestCase):
 
     def test_compare_ica(self):
-        S1 = pd.read_csv("data/mtb_S.csv", index_col=0)
-        S2 = pd.read_csv("data/ecoli_S.csv", index_col=0)
-        ortho_dir = "/home/sbrg/Desktop/modulome/data/organism_compare/bbh_csv"
-        dot, links = compare_ica(S1, S2, ortho_dir=ortho_dir)
-        print(links)
-
-    def test_compare_ica_flip(self):
         S2 = pd.read_csv("data/mtb_S.csv", index_col=0)
         S1 = pd.read_csv("data/ecoli_S.csv", index_col=0)
-        ortho_dir = "/home/sbrg/Desktop/modulome/data/organism_compare/bbh_csv"
-        dot, links = compare_ica(S1, S2, ortho_dir=ortho_dir)
-        print(links)
-
-    def test_compare_ica_manual(self):
-        S2 = pd.read_csv("data/mtb_S.csv", index_col=0)
-        S1 = pd.read_csv("data/ecoli_S.csv", index_col=0)
-        ortho_dir = "/home/sbrg/Desktop/modulome/data/organism_compare/bbh_csv"
-        dot, links = compare_ica(S1, S2, ortho_dir=ortho_dir, auto_find=False, org_1_name="eColi",
-                                 org_2_name="mTuberculosis")
-
+        ortho_dir = "/home/sbrg/Desktop/modulome/data/organism_compare/" \
+                    "bbh_csv/eColi_full_protein_vs" \
+                    "_mTuberculosis_full_protein_parsed.csv"
+        dot, links = compare_ica(S1, S2, ortho_file=ortho_dir)
         print(links)
 
 
