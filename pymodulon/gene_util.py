ce25afc55dd305ca82f4b1ec107639a4b9e90f0f
avsastry <avsastry@eng.ucsd.edu>
2020-09-15 15:31:44 -0700

    fix: COG annotation now uses full description

--- pymodulon/gene_util.py
+++ pymodulon/gene_util.py
@@ -8,6 +8,47 @@ from io import StringIO
 import urllib
 
 
+def cog2str(cog):
+    """
+    cog2str: Get the description for a COG category letter
+    Args:
+        cog: COG category letter
+
+    Returns:
+        Description of COG category
+    """
+    cog_dict = {
+        'A': 'RNA processing and modification',
+        'B': 'Chromatin structure and dynamics',
+        'C': 'Energy production and conversion',
+        'D': 'Cell cycle control, cell division, chromosome partitioning',
+        'E': 'Amino acid transport and metabolism',
+        'F': 'Nucleotide transport and metabolism',
+        'G': 'Carbohydrate transport and metabolism',
+        'H': 'Coenzyme transport and metabolism',
+        'I': 'Lipid transport and metabolism',
+        'J': 'Translation, ribosomal structure and biogenesis',
+        'K': 'Transcription',
+        'L': 'Replication, recombination and repair',
+        'M': 'Cell wall/membrane/envelope biogenesis',
+        'N': 'Cell motility',
+        'O': 'Post-translational modification, protein turnover, and chaperones',
+        'P': 'Inorganic ion transport and metabolism',
+        'Q': 'Secondary metabolites biosynthesis, transport, and catabolism',
+        'R': 'General function prediction only',
+        'S': 'Function unknown',
+        'T': 'Signal transduction mechanisms',
+        'U': 'Intracellular trafficking, secretion, and vesicular transport',
+        'V': 'Defense mechanisms',
+        'W': 'Extracellular structures',
+        'X': 'No COG annotation',
+        'Y': 'Nuclear structure',
+        'Z': 'Cytoskeleton'
+    }
+
+    return cog_dict[cog]
+
+
 def _get_attr(attributes, attr_id, ignore=False):
     """
     Helper function for parsing GFF annotations
