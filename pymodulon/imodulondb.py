d4c8337d8c0b4fae943ab09d431acb4d838738e3
avsastry <avsastry@eng.ucsd.edu>
2020-12-23 10:18:02 -0800

    refactor: Reverted COGs back to the dictionary from 2ad3e7f0

--- pymodulon/imodulondb.py
+++ pymodulon/imodulondb.py
@@ -868,7 +868,41 @@ def _gene_color_dict(model: IcaData):
     try:
         return {k: model.cog_colors[v] for k, v in gene_cogs.items()}
     except (KeyError, AttributeError):
-        set(gene_cogs.values()) - set(model.cog_colors.keys())
+        # previously, this would call the setter using:
+        # model.cog_colors = None
+        cogs = sorted(model.gene_table.COG.unique())
+        model.cog_colors = dict(
+            zip(
+                cogs,
+                [
+                    "red",
+                    "pink",
+                    "y",
+                    "orchid",
+                    "mediumvioletred",
+                    "green",
+                    "lightgray",
+                    "lightgreen",
+                    "slategray",
+                    "blue",
+                    "saddlebrown",
+                    "turquoise",
+                    "lightskyblue",
+                    "c",
+                    "skyblue",
+                    "lightblue",
+                    "fuchsia",
+                    "dodgerblue",
+                    "lime",
+                    "sandybrown",
+                    "black",
+                    "goldenrod",
+                    "chocolate",
+                    "orange",
+                ],
+            )
+        )
+        return {k: model.cog_colors[v] for k, v in gene_cogs.items()}
 
 
 def imdb_gene_scatter_df(
