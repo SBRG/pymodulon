fix: Fixed bug in explained_variance that always resulted in 0 explained variance

src/pymodulon/util.py
@@ -252,7 +252,7 @@ def explained_variance(
         centered = ica_data.X.subtract(ica_data.X[reference].mean(axis=1), axis=0)
 
     # Account for normalization procedures before ICA (X=SA-x_mean)
-    baseline = centered.subtract(centered, axis=1)
+    baseline = centered.subtract(centered.mean(axis=0), axis=1)
     baseline = baseline.loc[genes, samples]
 
     # Initialize variables