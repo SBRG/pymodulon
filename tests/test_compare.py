22e508afe7060ba8b7246600c1af2fa903a4a9d8
sapoudel <sapoudel@ucsd.edu>
2021-03-16 09:23:51 -0700

    formatting changes with pre-commit

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -1,8 +1,10 @@
+import os
+
+import pytest
+
 import pymodulon.compare as cmp
 from pymodulon.core import IcaData
 from pymodulon.example_data import load_example_bbh, load_staph_data
-import pytest
-import os
 
 
 @pytest.fixture
@@ -30,8 +32,9 @@ def test_convert_gene_index(staph_obj, example_bbh, ecoli_obj):
     assert len(M1.index) == 10
 
     # Test conforming M matrix from different organisms
-    orgM1, orgM2 = cmp.convert_gene_index(ecoli_obj.M, staph_obj.M,
-                                          ortho_file=example_bbh)
+    orgM1, orgM2 = cmp.convert_gene_index(
+        ecoli_obj.M, staph_obj.M, ortho_file=example_bbh
+    )
     assert (orgM1.index == orgM2.index).all()
     assert len(orgM1) > 10
 
@@ -49,41 +52,47 @@ def test_convert_gene_index(staph_obj, example_bbh, ecoli_obj):
 # TODO: figure out how to silence this warning, this isnt working
 @pytest.mark.filterwarnings("ignore:BiopythonWarning")
 def test_make_prots_faout(tmp_path):
-    gbk = '../src/pymodulon/data/ecoli/genome.gb'
-    fa_out = tmp_path / 'test.fa'
+    gbk = "../src/pymodulon/data/ecoli/genome.gb"
+    fa_out = tmp_path / "test.fa"
     fa_out.touch()
     cmp.make_prots(gbk, fa_out)
 
     with open(fa_out) as fa:
         line = fa.readline().strip()
-        assert line == '>b0001'
+        assert line == ">b0001"
 
 
 # create single and multi-file params for test_make_prots_db
 
-single_file = {'fasta_file': '../src/pymodulon/data/ecoli/proteins.faa',
-               'outname': 'test_db.fa'}
-multi_file = {'fasta_file': ['../src/pymodulon/data/ecoli/proteins.faa',
-                             '../src/pymodulon/data/ecoli/truncated_proteins.faa'],
-              'outname': 'test_db.fa',
-              'combined': 'combined.fa'}
+single_file = {
+    "fasta_file": "../src/pymodulon/data/ecoli/proteins.faa",
+    "outname": "test_db.fa",
+}
+multi_file = {
+    "fasta_file": [
+        "../src/pymodulon/data/ecoli/proteins.faa",
+        "../src/pymodulon/data/ecoli/truncated_proteins.faa",
+    ],
+    "outname": "test_db.fa",
+    "combined": "combined.fa",
+}
 
-make_prots_test = [(single_file, 'test_db.fa.pin'),
-                   (multi_file, 'test_db.fa.pin')]
+make_prots_test = [(single_file, "test_db.fa.pin"), (multi_file, "test_db.fa.pin")]
 
 
 @pytest.mark.parametrize("arg, expected", make_prots_test)
 def test_make_prots_db(tmp_path, arg, expected):
     # update tmp_dirs for outputs
-    arg['outname'] = str(tmp_path / arg['outname'])
+    arg["outname"] = str(tmp_path / arg["outname"])
     expected = tmp_path / expected
     try:
-        arg['combined'] = str(tmp_path / arg['combined'])
+        arg["combined"] = str(tmp_path / arg["combined"])
     except KeyError:
         pass
 
     cmp.make_prot_db(**arg)
-    assert (os.path.isfile(expected))
+    assert os.path.isfile(expected)
+
 
 # def test_compare_ica():
 #     S2 = pd.read_csv("data/mtb_S.csv", index_col=0)
